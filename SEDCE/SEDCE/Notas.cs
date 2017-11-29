8b5eac970cd8c65d5246846265fb7bde1dadcad8
Jonathan Rodriguez <j.ona.0412@hotmail.com>
2017-11-29 01:25:11 -0700

    Se modifico el formato de reporte

---
+++ SEDCE/SEDCE/Notas.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SEDCE
+{
+    public class Notas
+    {
+        //Notas importantes xd
+
+        //Procedure de reporte :v
+
+        //PROCEDURE [dbo].[MATRICULA_COMPLETA] AS
+        //SELECT sexo AS 'Genero', Años AS 'Edad', nombre_carrera AS 'Carrera', 
+        //       semestre as'Semestre', competencias as 'Competencia',
+        //       null as 'Extanjero',lengua_indigena as 'Lengua_Indigena',
+        //       discapacidad as 'Discapacidad' ,promediodecalificacion as 'Promedio',
+        //       Count(no_de_control) AS Cantidad
+        //FROM ALUMNO INNER JOIN carrera 
+        //ON ALUMNO.id_carrera = carrera.id_carrera 
+        //Group by sexo, AÑOS, nombre_carrera,semestre,COMPETENCIAs, Lengua_Indigena, Discapacidad, PromedioDecalificacion
+        //Order by carrera
+    }
+}
