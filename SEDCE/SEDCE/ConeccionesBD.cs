c8222f7ef82731e19957151c04d10f6a7dfb78de
Jonathan Rodriguez <j.ona.0412@hotmail.com>
2017-12-03 21:24:14 -0700

    Se agrego la carga de archivos

--- SEDCE/SEDCE/ConeccionesBD.cs
+++ SEDCE/SEDCE/ConeccionesBD.cs
@@ -59,5 +59,29 @@ namespace SEDCE
             dt.Load(cmd.ExecuteReader());
             return dt;
         }
+
+        public double IDCarrera(string carrera) 
+        {
+            string resultado = "";
+            switch (carrera)
+            {
+                case "INGENIERIA MECANICA": resultado = "5"; break;
+                case "INGENIERIA ELECTRICA": resultado = "6"; break;
+                case "INGENIERIA INDUSTRIAL": resultado = "8"; break;
+                case "INGENIERIA EN SISTEMAS COMPUTACIONALES": resultado = "10"; break;
+                case "INGENIERIA ELECTRONICA": resultado = "11"; break;
+                case "LICENCIATURA EN ADMINISTRACION": resultado = "16"; break;
+                case "MAESTRIA EN ADMINISTRACION": resultado = "24"; break;
+                case "MAESTRIA EN ELECTRONICA": resultado = "27"; break;
+                case "MAESTRIA EN CIENCIAS DE LA COMPUTACION": resultado = "28"; break;
+                case "MAESTRIA EN INGENIERIA INDUSTRIAL": resultado = "29"; break;
+                case "INGENIERIA MECATRONICA": resultado = "31"; break;
+                case "INGENIERIA EN GESTION EMPRESARIAL": resultado = "32"; break;
+                case "INGENIERIA EN INFORMATICA": resultado = "33"; break;
+                case "INGENIERIA BIOMEDICA": resultado = "34"; break;
+                case "ING. IND. 100 INGLES": resultado = "35"; break;
+            }
+            return Convert.ToDouble(resultado);
+        }
     }
 }
